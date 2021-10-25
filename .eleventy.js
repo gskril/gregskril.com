78547967f648cd375a57437d96576ab775825218
Greg <gskril@gmail.com>
2021-10-25 12:42:53 -0400

    Change all html files to Nunjucks for 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,10 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("img")
 	eleventyConfig.addPassthroughCopy("css")
 
+	eleventyConfig.addFilter("limit", function (arr, limit) {
+		return arr.slice(0, limit)
+	})
+
 	return {
 		dir: {
 			input: "src",
