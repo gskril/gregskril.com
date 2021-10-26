9c8fea098cb3502d524a21b22d7a1aa99c8256d9
Greg <gskril@gmail.com>
2021-10-26 00:38:47 -0400

    Fix multi-line underline CSS bug, add read time

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
+const readingTime = require("eleventy-plugin-reading-time")
+
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("img")
 	eleventyConfig.addPassthroughCopy("css")
+
+	eleventyConfig.addPlugin(readingTime)
 
 	eleventyConfig.addFilter("limit", function (arr, limit) {
 		return arr.slice(0, limit)
