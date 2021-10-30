74d5adfb1bb5304eedf74f7648371b5ab7d51440
Greg <gskril@gmail.com>
2021-10-30 19:29:43 -0400

    Optimize links in blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,30 @@
-const readingTime = require("eleventy-plugin-reading-time")
-
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("img")
 	eleventyConfig.addPassthroughCopy("css")
-
+	
+	// Add pluginf or reading time on blog posts
+	const readingTime = require("eleventy-plugin-reading-time")
 	eleventyConfig.addPlugin(readingTime)
 
+	// Optimize generated HTML links
+	const markdownIt = require("markdown-it")
+
+	const markdownItOptions = {
+		html: true,
+	}
+	const mila = require("markdown-it-link-attributes")
+	const milaOptions = {
+		pattern: /^https:/,
+		attrs: {
+			target: "_blank",
+			rel: "noopener",
+		},
+	}
+	
+	const markdownLib = markdownIt(markdownItOptions).use(mila, milaOptions)
+	eleventyConfig.setLibrary("md", markdownLib)
+
+	// Add filter to limit number of posts shown on collection pages
 	eleventyConfig.addFilter("limit", function (arr, limit) {
 		return arr.slice(0, limit)
 	})
