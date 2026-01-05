960c005031cd8d9564f6f891c21a4b5293489142
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2026-01-05 16:05:08 -0500

    Migrate RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gregskril.com',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 })
